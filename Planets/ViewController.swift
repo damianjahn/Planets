00cf2fc8fc3ca7c9e9de1303f3a3618c847be19d
Damian <damian.jahn@icloud.com>
2020-02-11 18:29:58 +0100

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Damian Jahn on 11/02/2020.
+//  Copyright © 2020 Damian Jahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
