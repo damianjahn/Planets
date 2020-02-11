bcabd8a8ddb056aec69dfcff8f5a9b99526ced29
Damian <damian.jahn@icloud.com>
2020-02-11 19:22:27 +0100

    A planets app in ARKit

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.sceneView.automaticallyUpdatesLighting = true
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Earth day")
+        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "Earth Specular")
+        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "Earth Emission")
+        earth.geometry?.firstMaterial?.normal.contents = UIImage(named: "Earth Normal")
+        earth.position = SCNVector3(0,0, -1)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
     }
 
 
+}
+
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
